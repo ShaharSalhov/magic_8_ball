f09c7e022a1d007fa446d2b711143af05a0a58fd
ShaharSalhov <shaharsalhov0@gmail.com>
2022-06-14 11:31:04 +0300

    background and font

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const answers = ["No", "Yes", "Most likely", "Not sure", "Don't count on it", "You can count on it", "Absolutely", "Concentrate and ask again", "Ask again in a few minutes", "Better not to tell you now"]
 
 function App() {
+
+const [answer, setAnswer] = React.useState("")
+
+const handleClick = () => {
+  getRandomAnswer()
+}
+
+const getRandomAnswer = () => {
+  setAnswer(answers[Math.floor(Math.random()*answers.length)])
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <h4> They all come to me... seeking advice ...</h4>
+        <h6> Ask the magic 8-ball a question and shake it</h6>
+
+        <img src={"../public/ballPic.png"} className="pic" alt="pic" />
+
+        <p> {answer} </p>
+
+        <button onClick={handleClick}> Shake </button>
+
     </div>
   );
 }
