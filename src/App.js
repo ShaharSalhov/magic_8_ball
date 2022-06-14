83abd9ef0afdfbd62441c718555cf6b0eab1ad18
ShaharSalhov <shaharsalhov0@gmail.com>
2022-06-14 20:31:54 +0300

    toy div and ball div

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 
-const answers = ["No", "Yes", "Most likely", "Not sure", "Don't count on it", "You can count on it", "Absolutely", "Concentrate and ask again", "Ask again in a few minutes", "Better not to tell you now"]
+const answers = ["No", "Yes", "Most likely", "Not sure", "Don't count on it", "You can count on it", "Absolutely", "Concentrate and ask again", "Ask again in a few minutes", "Better not to tell you now", "It is certain"]
 
 function App() {
 
@@ -18,17 +18,29 @@ const getRandomAnswer = () => {
   return (
     <div className="App">
 
-        <h4> They all come to me... seeking advice ...</h4>
-        <h6> Ask the magic 8-ball a question and shake it</h6>
+      <h4> They all come to me... seeking advice ...</h4>
 
-        <img src={"../public/ballPic.png"} className="pic" alt="pic" />
+      <h6> If you're brave enough to know your fortune, ask the magic 8-ball a question and shake it... </h6>
 
-        <p> {answer} </p>
+      <div className='magicalToy'>
 
-        <button onClick={handleClick}> Shake </button>
+            <div className="ball">
+              <div className="eightCircle">
+                <div className="triangle">
+                  <p className='text'>{answer}</p>
+                </div>
+              </div>
+          
+          <div className="shadow"></div>
+
+          </div>
+      </div>
+
+      <button onClick={handleClick}> Shake </button>
 
     </div>
   );
 }
 
 export default App;
+
